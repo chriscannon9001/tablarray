6196639b2e4754224678a9ddfc8fa4fdd7f9d653
chris <chriscannon9001@noreply>
2021-01-12 19:27:18 -0800

    Added multi-level indexing to Assembly, some general renaming, addition of isassembly and istablarray functions.

--- base.py
+++ base.py
@@ -9,16 +9,21 @@ Created on Sun Jan  3 15:34:31 2021
 """
 
 
-def quackslike_Tablarray(obj):
+def istablarray(obj):
     return hasattr(obj, 'ts') and hasattr(obj, 'view')
 
 
-def rval_once_a_ta(rclass, rval, cdim, view):
+def isassembly(a):
+    return (hasattr(a, '_tablarrays') and hasattr(a, '_ts')
+            and hasattr(a, 'keys'))
+
+
+def _rval_once_a_ta(rclass, rval, cdim, view):
     """"""
     if rval.ndim == cdim:
         return rval
     return rclass(rval, cdim, view)
 
 
-def rval_always_ta(rclass, rval, cdim, view):
+def _rval_always_ta(rclass, rval, cdim, view):
     return rclass(rval, cdim, view)
